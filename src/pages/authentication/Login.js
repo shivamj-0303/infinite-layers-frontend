74a02926baa6bd31b4955c775fff641e6d0119bd
shivamj-0303 <shivamjangid0108@gmail.com>
2026-05-16 17:06:32 +0530

    refactor: remove unused JWT decode function from Login component

--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -70,23 +70,6 @@ function Login({ onLoginSuccess }) {
     }
   };
 
-  // Simple JWT decode function
-  const parseJwt = (token) => {
-    try {
-      const base64Url = token.split('.')[1];
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      const jsonPayload = decodeURIComponent(
-        atob(base64)
-          .split('')
-          .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-          .join('')
-      );
-      return JSON.parse(jsonPayload);
-    } catch (e) {
-      return { email };
-    }
-  };
-
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Back Button */}
